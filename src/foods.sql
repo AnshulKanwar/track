875b93713d4427a6293c159f709f10962c53c5ef
Anshul Kanwar <4n5hu1@protonmail.com>
2022-09-11 09:51:11 +0530

    Fixed foods.sql syntax error

--- src/foods.sql
+++ src/foods.sql
@@ -1,5 +1,11 @@
 INSERT INTO food (name, slug, calories, carbohydrates, fat, protein)
 VALUES
+    ('Banana', 'banana', 105, 27, 0.4, 1.3),
     ('Boiled Eggs', 'boiled_eggs', 78, 0.6, 5.4, 6.3),
+    ('Noodles', 'noodles', 143, 22.1, 4.5, 3.8),
+    ('Protein Shake', 'protein_shake', 99, 2.1, 1.5, 19),
+    ('Rajma', 'rajma', 121, 15.2, 4.5, 5.2),
+    ('Rice', 'rice', 120, 26.7, 0.3, 2.6),
     ('Roti', 'roti', 120, 18, 3.7, 3.1),
-    ('Protein Shake', 'protein_shake', 99, 2.1, 1.5, 19);
+    ('Steamed Chicken', 'steamed_chicken', 259, 0.3, 1.5, 61.3),
+    ('Vanilla Ice Cream', 'vanilla_ice_cream', 168, 21.5, 7.5, 3.5);
